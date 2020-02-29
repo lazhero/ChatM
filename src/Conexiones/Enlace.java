6b346a523d1c13203466de6d0e6afe1fd6d9fc83
lazhero <54720338+lazhero@users.noreply.github.com>
2020-02-29 11:32:29 -0600

    #2 Se importan los modulos de comunicacion de repositorio anterior

---
+++ src/Conexiones/Enlace.java
@@ -0,0 +1,93 @@
+package Conexiones;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+public class Enlace {
+    private  int port;
+    private  String ip;
+    private Socket client;
+    private boolean ConectadoS=false;
+    public Enlace(int port, String ip){
+        this.port=port;
+        this.ip=ip;
+    }
+    public Enlace(){
+      this.port=40000;
+      this.ip="127.0.0.1";
+    }
+    public Enlace(String ip){
+        this.ip=ip;
+        this.port=40000;
+    }
+    public Enlace(int port){
+        this.port=port;
+        this.ip="127.0.0.1";
+    }
+
+    public void ConectarEnviarFijo(){
+        try{
+            Socket Receptor=new Socket(this.ip,this.port);
+            System.out.println("Conexion Exitosa");
+            this.client= Receptor;
+
+
+        }
+        catch(Exception excep){
+            System.out.print("El puerto no esta disponible");
+            this.ConectarEnviarVariable();
+        }
+    }
+    public void ConectarEnviarVariable(){
+        this.port=40000;
+        Socket Receptor=null;
+        boolean flag=true;
+        while(this.port<40100 && flag) {
+            try {
+                Receptor = new Socket(this.ip,this.port);
+                this.ConectadoS=true;
+                System.out.println("Puerto encontrado");
+                flag=false;
+            } catch (IOException excep) {
+                this.port++;
+                Receptor=null;
+
+            }
+        }
+        this.client= Receptor;
+    }
+    public void ConectarEnviarVariable(int inicio){
+        this.port=inicio;
+        Socket Receptor=null;
+        boolean flag=true;
+        while(this.port<40100 && flag) {
+            try {
+                Receptor = new Socket(this.ip,this.port);
+                this.ConectadoS=true;
+                System.out.println("Puerto encontrado");
+                flag=false;
+            } catch (IOException excep) {
+                this.port++;
+                Receptor=null;
+
+            }
+        }
+        this.client= Receptor;
+    }
+    public void EnviarMensaje(String Mensaje){
+        try {
+            OutputStreamWriter Escritura = new OutputStreamWriter(this.client.getOutputStream());
+            Escritura.write(Integer.toString(this.port)+"~"+Mensaje + "\n");
+            Escritura.flush();
+            this.client.close();
+        }
+        catch(IOException excep){
+            System.out.print("Error");
+        }
+    }
+    public int getPort(){
+        return this.port;
+    }
+
+}
---
+++ src/Conexiones/EnlaceServidores.java
@@ -0,0 +1,84 @@
+package Conexiones;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EnlaceServidores {
+    private  int port;
+    private  String ip;
+    private boolean ConectadoS=false;
+    private ServerSocket Server;
+    public EnlaceServidores(int port, String ip){
+        this.port=port;
+        this.ip=ip;
+    }
+    public EnlaceServidores(){
+        this.port=4000;
+        this.ip="127.0.0.1";
+    }
+    public EnlaceServidores(String ip){
+        this.ip=ip;
+        this.port=4000;
+    }
+    public EnlaceServidores(int port){
+        this.port=port;
+        this.ip="127.0.0.1";
+    }
+    public void ConectarRecepFijo(){
+        try{
+            this.Server=new ServerSocket(this.port);
+            System.out.println("Conexion Exitosa");
+        }
+        catch(IOException excep){
+            System.out.println("El puerto no esta disponible");
+            this.ConectarRecepVariable();
+        }
+    }
+    public void ConectarRecepVariable(){
+        this.port=40000;
+        this.Server=null;
+        boolean flag=true;
+        while(this.port<40100 && flag) {
+            try {
+                this.Server = new ServerSocket(this.port);
+                ServerSocket Prueba=new ServerSocket(this.port+1);
+                System.out.println("Conexion Exitosa");
+                flag=false;
+                this.ConectadoS=true;
+            } catch (IOException excep) {
+                this.port++;
+
+            }
+        }
+
+    }
+    public String[] RecibirMensaje(){
+        String Texto="";
+        String[] retorno=null;
+        boolean Recibiendo=true;
+        try {
+            while (Recibiendo) {
+                Socket Entrada = this.Server.accept();
+                BufferedReader Lector = new BufferedReader(new InputStreamReader(Entrada.getInputStream()));
+                Texto = Lector.readLine();
+                if(RevisionFormato.Format(Texto,"~")){
+                    retorno=Texto.split("~");
+                    Recibiendo=false;
+
+
+                }
+            }
+
+        }
+        catch(IOException Exp){
+            System.out.print("Matenme");
+        }
+        return retorno;
+
+    }
+    public int getPort(){
+        return this.port;
+    }
+}
