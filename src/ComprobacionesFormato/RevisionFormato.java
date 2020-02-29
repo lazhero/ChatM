ee4161716938525bb98527ff5cb27fdba622cc13
lazhero <54720338+lazhero@users.noreply.github.com>
2020-02-29 11:34:42 -0600

    #3 Se crea la clase para la revision del formato de los mensajes de texto

---
+++ src/ComprobacionesFormato/RevisionFormato.java
@@ -0,0 +1,16 @@
+package ComprobacionesFormato;
+
+public class RevisionFormato {
+    public static boolean Format(String texto,String Subtexto){
+      int apperance=texto.indexOf(Subtexto);
+      String[] divisiones;
+      if (apperance>=0){
+          divisiones=texto.split(Subtexto);
+          if (divisiones.length==2)return true;
+          else return false;
+      }
+      else return false;
+    }
+
+}
+
